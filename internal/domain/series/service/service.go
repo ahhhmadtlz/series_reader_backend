5f3aeba7aabc35f8aeebf6284aca2ce064aaa76c
ahmad <talezariahmad00@gmail.com>
2026-02-09 11:11:52 +0330

    refactor:refactor(service): depend on repository interface instead of concrete type

--- internal/domain/series/service/service.go
+++ internal/domain/series/service/service.go
@@ -1,29 +1,19 @@
 package service
 
 import (
-	"context"
 	"time"
 
 	"github.com/ahhhmadtlz/series_reader_backend/internal/domain/series/entity"
 	"github.com/ahhhmadtlz/series_reader_backend/internal/domain/series/param"
+	"github.com/ahhhmadtlz/series_reader_backend/internal/domain/series/repository"
 )
 
-type Repository interface {
-	Create(ctx context.Context, series entity.Series) (entity.Series, error)
-	GetByID(ctx context.Context, id uint) (entity.Series, error)
-	GetBySlug(ctx context.Context, slug string) (entity.Series, error)
-	GetList(ctx context.Context, req param.GetListRequest) ([]entity.Series, int, error)
-	Update(ctx context.Context, id uint, series entity.Series) (entity.Series, error)
-	Delete(ctx context.Context, id uint) error
-	IncrementViewCount(ctx context.Context, id uint) error
-	IsSlugExists(ctx context.Context, slug string) (bool, error)
-}
 
 type Service struct {
-	repo Repository
+	repo repository.Repository
 }
 
-func New(repo Repository) Service {
+func New(repo repository.Repository) Service {
 	return Service{
 		repo: repo,
 	}
