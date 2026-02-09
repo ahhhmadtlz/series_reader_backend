ced7df6646d0150b636b29c0e905060c993041ca
ahmad <talezariahmad00@gmail.com>
2026-02-09 11:09:34 +0330

    feat: implement httpserver and healthcheck route for test

--- go.mod
+++ go.mod
@@ -8,13 +8,30 @@ require (
 )
 
 require (
+	github.com/go-gorp/gorp/v3 v3.1.0 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/crypto v0.46.0 // indirect
+	golang.org/x/net v0.48.0 // indirect
+	golang.org/x/time v0.14.0 // indirect
+)
+
+require (
 	github.com/fatih/structs v1.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
+	github.com/labstack/echo/v4 v4.15.0
+	github.com/lib/pq v1.11.1
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
-	golang.org/x/sys v0.0.0-20210603081109-ebe580a85c40 // indirect
+	github.com/rubenv/sql-migrate v1.8.1
+	go.uber.org/zap v1.27.1
+	golang.org/x/sys v0.39.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/delivery/httpserver/server.go
+++ internal/delivery/httpserver/server.go
@@ -1,1 +1,91 @@
 package httpserver
+
+import (
+	"fmt"
+
+	"github.com/ahhhmadtlz/series_reader_backend/internal/config"
+	"github.com/ahhhmadtlz/series_reader_backend/internal/observability/logger"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Server struct {
+	config config.Config
+	Router *echo.Echo
+}
+
+// New creates a new HTTP server
+func New(cfg config.Config) *Server {
+	return &Server{
+		Router: echo.New(),
+		config: cfg,
+	}
+}
+
+func (s *Server) Serve() {
+	// Setup middleware
+	s.Router.Use(middleware.Recover())
+	s.Router.Use(middleware.RequestID())
+
+	// Request logger middleware using global logger
+	s.Router.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogURI:           true,
+		LogStatus:        true,
+		LogHost:          true,
+		LogRemoteIP:      true,
+		LogRequestID:     true,
+		LogMethod:        true,
+		LogContentLength: true,
+		LogResponseSize:  true,
+		LogLatency:       true,
+		LogError:         true,
+		LogProtocol:      true,
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			if v.Error == nil {
+				logger.Info("request",
+					"request_id", v.RequestID,
+					"host", v.Host,
+					"content_length", v.ContentLength,
+					"protocol", v.Protocol,
+					"method", v.Method,
+					"latency", v.Latency,
+					"remote_ip", v.RemoteIP,
+					"response_size", v.ResponseSize,
+					"uri", v.URI,
+					"status", v.Status,
+				)
+			} else {
+				logger.Error("request",
+					"request_id", v.RequestID,
+					"host", v.Host,
+					"content_length", v.ContentLength,
+					"protocol", v.Protocol,
+					"method", v.Method,
+					"latency", v.Latency,
+					"error", v.Error.Error(),
+					"remote_ip", v.RemoteIP,
+					"response_size", v.ResponseSize,
+					"uri", v.URI,
+					"status", v.Status,
+				)
+			}
+			return nil
+		},
+	}))
+
+	// Health check endpoint
+	s.Router.GET("/health-check", s.healthCheck)
+
+	// Start server
+	address := fmt.Sprintf(":%d", s.config.HTTPServer.Port)
+	
+	logger.Info("Starting Echo server",
+		"address", address,
+	)
+
+	if err := s.Router.Start(address); err != nil {
+		logger.Error("Failed to start server",
+			"error", err.Error(),
+		)
+	}
+}
