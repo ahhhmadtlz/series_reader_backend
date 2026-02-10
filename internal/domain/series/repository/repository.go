e205b896dc2ebdfff34f8694fa2548d72551cf4f
ahmad <talezariahmad00@gmail.com>
2026-02-10 16:17:59 +0330

    chore: rename GetSlug to GetByFullSlug

--- internal/domain/series/repository/repository.go
+++ internal/domain/series/repository/repository.go
@@ -10,7 +10,7 @@ import (
 type Repository interface {
 	Create(ctx context.Context, series entity.Series)(entity.Series,error)
 	GetByID(ctx context.Context,id uint)(entity.Series,error)
-	GetBySlug(ctx context.Context,slug string)(entity.Series,error)
+	GetByFullSlug(ctx context.Context,slug string)(entity.Series,error)
 	GetList(ctx context.Context,req param.GetListRequest)([]entity.Series,int,error)
 	Update(ctx context.Context,id uint,series entity.Series)(entity.Series,error)
 	Delete(ctx context.Context,id uint)error
