26bdc1215fa9ad65722e1c6ea4ca0f3d4a6b2b26
ahmad <talezariahmad00@gmail.com>
2026-02-13 15:47:25 +0330

    refactor: add explicit type for seriesService

--- internal/delivery/httpserver/serieshandler/handler.go
+++ internal/delivery/httpserver/serieshandler/handler.go
@@ -1,16 +1,16 @@
 package serieshandler
 
 import (
-	"github.com/ahhhmadtlz/series_reader_backend/internal/domain/series/service"
+	seriesService "github.com/ahhhmadtlz/series_reader_backend/internal/domain/series/service"
 	"github.com/ahhhmadtlz/series_reader_backend/internal/domain/series/validator"
 )
 
 type Handler struct {
-	service service.Service
+	service seriesService.Service
 	validator validator.Validator
 }
 
-func New(service service.Service,validator validator.Validator)Handler {
+func New(service seriesService.Service,validator validator.Validator)Handler {
 	return Handler{
 		service: service,
 		validator: validator,
