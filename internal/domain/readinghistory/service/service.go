e366cceb567b9c8b110e68274c4dc97db71a71b9
ahmad <talezariahmad00@gmail.com>
2026-02-26 19:07:59 +0330

    fix(readinghistory): replace cross-domain repo imports with local interfaces

--- internal/domain/readinghistory/service/service.go
+++ internal/domain/readinghistory/service/service.go
@@ -1,21 +1,37 @@
 package service
 
 import (
-	chapterRepository "github.com/ahhhmadtlz/series_reader_backend/internal/domain/chapter/repository"
+	"context"
+
+	chapterEntity "github.com/ahhhmadtlz/series_reader_backend/internal/domain/chapter/entity"
 	readinghistoryRepository "github.com/ahhhmadtlz/series_reader_backend/internal/domain/readinghistory/repository"
-	seriesRepository "github.com/ahhhmadtlz/series_reader_backend/internal/domain/series/repository"
+	seriesEntity "github.com/ahhhmadtlz/series_reader_backend/internal/domain/series/entity"
 )
+
+type ChapterRepository interface {
+	GetByID(ctx context.Context, id uint) (*chapterEntity.Chapter, error)
+	GetBySeriesID(ctx context.Context, seriesID uint) ([]*chapterEntity.Chapter, error)
+}
+
+type SeriesRepository interface {
+	GetByID(ctx context.Context, id uint) (seriesEntity.Series, error)
+	GetByFullSlug(ctx context.Context, slug string) (seriesEntity.Series, error)
+}
 
 type Service struct {
 	readinghistoryRepo readinghistoryRepository.Repository
-	chapterRepo chapterRepository.Repository
-	seriesRepo  seriesRepository.Repository
+	chapterRepo        ChapterRepository
+	seriesRepo         SeriesRepository
 }
 
-func New(readinghistoryRepo readinghistoryRepository.Repository, chapterRepo chapterRepository.Repository, seriesRepo seriesRepository.Repository) Service {
+func New(
+	readinghistoryRepo readinghistoryRepository.Repository,
+	chapterRepo ChapterRepository,
+	seriesRepo SeriesRepository,
+) Service {
 	return Service{
-	  readinghistoryRepo:  readinghistoryRepo,
-		chapterRepo: chapterRepo,
-		seriesRepo:  seriesRepo,
+		readinghistoryRepo: readinghistoryRepo,
+		chapterRepo:        chapterRepo,
+		seriesRepo:         seriesRepo,
 	}
 }
