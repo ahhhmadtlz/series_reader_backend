03776960b440380cf1633852bbe0631df609fc19
ahmad <talezariahmad00@gmail.com>
2026-02-26 19:08:30 +0330

    fix(upload): replace cross-domain repo imports with local interfaces

--- internal/domain/upload/service/service.go
+++ internal/domain/upload/service/service.go
@@ -1,28 +1,44 @@
 package service
 
 import (
-	seriesrepo "github.com/ahhhmadtlz/series_reader_backend/internal/domain/series/repository"
-	chapterrepo "github.com/ahhhmadtlz/series_reader_backend/internal/domain/chapter/repository"
+	"context"
+
+	chapterEntity "github.com/ahhhmadtlz/series_reader_backend/internal/domain/chapter/entity"
+	seriesEntity "github.com/ahhhmadtlz/series_reader_backend/internal/domain/series/entity"
 	"github.com/ahhhmadtlz/series_reader_backend/internal/domain/upload/repository"
-	userrepo "github.com/ahhhmadtlz/series_reader_backend/internal/domain/user/repository"
+	userEntity "github.com/ahhhmadtlz/series_reader_backend/internal/domain/user/entity"
 	"github.com/ahhhmadtlz/series_reader_backend/internal/infrastructure/storage"
 	"github.com/ahhhmadtlz/series_reader_backend/internal/infrastructure/worker"
 )
 
+type UserRepository interface {
+	GetUserByID(ctx context.Context, userID uint) (userEntity.User, error)
+	UpdateUser(ctx context.Context, user userEntity.User) (userEntity.User, error)
+}
+
+type SeriesRepository interface {
+	GetByID(ctx context.Context, id uint) (seriesEntity.Series, error)
+	Update(ctx context.Context, id uint, series seriesEntity.Series) (seriesEntity.Series, error)
+}
+
+type ChapterRepository interface {
+	GetByID(ctx context.Context, id uint) (*chapterEntity.Chapter, error)
+}
+
 type Service struct {
 	uploadRepo  repository.Repository
-	userRepo    userrepo.Repository
-	seriesRepo  seriesrepo.Repository
-	chapterRepo chapterrepo.Repository
+	userRepo    UserRepository
+	seriesRepo  SeriesRepository
+	chapterRepo ChapterRepository
 	storage     storage.Storage
 	jobQueue    worker.JobQueue
 }
 
 func New(
 	uploadRepo repository.Repository,
-	userRepo userrepo.Repository,
-	seriesRepo seriesrepo.Repository,
-	chapterRepo chapterrepo.Repository,
+	userRepo UserRepository,
+	seriesRepo SeriesRepository,
+	chapterRepo ChapterRepository,
 	storage storage.Storage,
 	jobQueue worker.JobQueue,
 ) Service {
